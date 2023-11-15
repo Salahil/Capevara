6039c9adb212aacb8382cbdd30bfcf5b5f2e7646
MacacoSemBrasso <julioykawahata@gmail.com>
2023-11-15 15:01:41 -0300

    Spring Boot initialize a quest foi passada, o Mago, o Paladino e a Curandeira partem para enfrentar oRei Orc. O caminho será árduo, com desfios nunca antes vistos... desejamos sorte para os bravos herois pelo vale de Spring Boot.

---
+++ Capevara_System/src/main/java/com/capevara/Capevara_System/CapevaraSystemApplication.java
@@ -0,0 +1,13 @@
+package com.capevara.Capevara_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapevaraSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapevaraSystemApplication.class, args);
+	}
+
+}
---
+++ Capevara_System/src/test/java/com/capevara/Capevara_System/CapevaraSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capevara.Capevara_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapevaraSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
