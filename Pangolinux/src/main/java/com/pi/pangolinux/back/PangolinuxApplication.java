ef2138036b5b7e9844fb984bcef0b186dde0bfa4
Julio Yushi Kawahata <104476454+Salahil@users.noreply.github.com>
2023-11-20 12:39:20 -0300

    Mudou Tudo! A casa caiu pra tu capevara!
    Ninguém gostou de vc capevara... todo mundo preferiu o Pangolinux!

---
+++ Pangolinux/src/main/java/com/pi/pangolinux/back/PangolinuxApplication.java
@@ -0,0 +1,13 @@
+package com.pi.pangolinux.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangolinuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangolinuxApplication.class, args);
+	}
+
+}
