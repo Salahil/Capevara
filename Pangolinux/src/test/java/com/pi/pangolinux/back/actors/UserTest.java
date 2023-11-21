f27b6ec1bb38af08b78a8502e541be16a0ac111d
MacacoSemBrasso <julioykawahata@gmail.com>
2023-11-21 11:03:51 -0300

    Istalado o Server Local O bardo chega as terras abandoadas da bota de Springs, e se depara com seu primeiro desafio... um sabio gato gigante e gordo chamado, 'Tomcat' ele é mortal e seu desejo é ser cortejado como um ser superior que receberá varias coisas, como uma tela, um user e se conectar com um BD e com um backend... o Bardo, miraculoso do jeito que ele é, consegue e escreve aqui os registros de suas aventuras...

---
+++ Pangolinux/src/test/java/com/pi/pangolinux/back/actors/UserTest.java
@@ -0,0 +1,14 @@
+package com.pi.pangolinux.back.actors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
