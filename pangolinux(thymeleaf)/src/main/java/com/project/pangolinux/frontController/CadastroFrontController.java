07f9eeaede5f33c8efac85b26374f54fea9ea147
SergioS0ares <sergyn200295@gmail.com>
2023-12-04 13:44:30 -0300

    houve alterações no controller
    não esta finalizado, mas ja possui um começo!

--- pangolinux(thymeleaf)/src/main/java/com/project/pangolinux/frontController/CadastroFrontController.java
+++ pangolinux(thymeleaf)/src/main/java/com/project/pangolinux/frontController/CadastroFrontController.java
@@ -1,22 +1,40 @@
 package com.project.pangolinux.frontController;
 
-import java.util.UUID;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.project.pangolinux.modelos.UsuarioModel;
+import com.project.pangolinux.repositorio.UsuarioRepository;
 
 @Controller
 public class CadastroFrontController {
-	
-	@GetMapping({"/cadastro", "/"})
-	public String telaCadastro() {
-		return "cadastro";
-	}	
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping({"/cadastro", "/"})
+    public String telaCadastro(Model model) {
+        model.addAttribute("usuario", new UsuarioModel());
+        return "cadastro";
+    }
+
+    @PostMapping("/cadastrarUsuario")
+    public String cadastrarUsuario(@ModelAttribute UsuarioModel usuario, Model model) {
+        // Lógica de validação
+        if (usuario.getCPF() == null || usuario.getCPF().isEmpty()) {
+            model.addAttribute("error", "CPF é obrigatório");
+            return "cadastro"; // Retorna para o formulário com mensagem de erro
+        }
+
+        // Lógica para salvar no banco de dados
+        usuarioRepository.save(usuario);
+
+        // Redireciona para a página de sucesso ou outra ação necessária
+        return "redirect:/pagina-de-sucesso";
+    }
 }
+
