f1a3a98647d97e890d0e29d6087c2d9f53ccbbb5
SergioS0ares <sergyn200295@gmail.com>
2023-12-05 20:55:52 -0300

    alterações rapidas

--- pangolinux(thymeleaf)/src/main/java/com/project/pangolinux/frontController/CriarDenunciaFrontController.java
+++ pangolinux(thymeleaf)/src/main/java/com/project/pangolinux/frontController/CriarDenunciaFrontController.java
@@ -1,30 +1,25 @@
 package com.project.pangolinux.frontController;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.project.pangolinux.modelos.DenunciaModel;
-import com.project.pangolinux.repositorio.DenunciaRepository;
-import com.project.pangolinux.repositorio.EnderecoRepository;
-import com.project.pangolinux.repositorio.UsuarioRepository;
 
 @Controller
 public class CriarDenunciaFrontController {
-	
-	@Autowired
-	DenunciaRepository repoDen;
-	
-	@Autowired
-	UsuarioRepository repoUser;
-	
-	@Autowired
-	EnderecoRepository repoEnd;
-	
-	@GetMapping("/denunciaUsuario")
-	public String telaCriarDenuncia(Model model) {
-		model.addAttribute("denuncia", new DenunciaModel());
-		return "denunciausuario";
-	}
+
+    @GetMapping("/denunciaUsuario")
+    public String mostrarFormularioDenuncia(Model model) {
+        
+        model.addAttribute("denuncia", new DenunciaModel());
+        return "denunciaUsuario"; 
+    }
+
+    @PostMapping("/salvarDenuncia")
+    public String salvarDenuncia(DenunciaModel denuncia) {
+        System.out.println("Denuncia salva: " + denuncia.toString());
+        return "redirect:/denunciaSalva"; 
+    }
 }
