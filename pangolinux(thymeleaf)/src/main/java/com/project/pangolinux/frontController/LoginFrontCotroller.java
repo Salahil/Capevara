70e7efe8050dd4a4f92d2dd7d4be387841692d9f
Salahil <julioykawahata@gmail.com>
2023-12-05 19:41:45 -0300

    commit desesperado 1.4

--- pangolinux(thymeleaf)/src/main/java/com/project/pangolinux/frontController/LoginFrontCotroller.java
+++ pangolinux(thymeleaf)/src/main/java/com/project/pangolinux/frontController/LoginFrontCotroller.java
@@ -1,14 +1,10 @@
 package com.project.pangolinux.frontController;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.pangolinux.modelos.UsuarioModel;
@@ -26,19 +22,16 @@ public class LoginFrontCotroller {
         return "login";
     }
 	
-	
-	
 	@PostMapping("/login")
     public String realizarLogin(@RequestParam String cpf, @RequestParam String senha, Model model) {
-		if (cpf == null) return "redirect:/denunciaUsuario";
+		if (cpf == null) return "redirect/denunciaUsuario";
 		UsuarioModel usuario = (UsuarioModel) repo.findByCPF(cpf);
-		
         if (usuario != null && usuario.getSenha().equals(senha)) {
-            model.addAttribute("mensagem", "Login bem-sucedido para o usuário: " + cpf);
+        	model.addAttribute("mensagem", "Login bem-sucedido para o usuário: " + cpf);
             if (!usuario.getTipoUsuario()) return "redirect/listaDenunciaUsuario";
             if (usuario.getTipoUsuario()) return "redirect/listaDenunciaAnalista";
         }
         model.addAttribute("mensagem", "Falha na autenticação. Verifique CPF e senha.");
-        return "login";    
+        return "redirect/login";
     }
 }
